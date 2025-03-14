7a92de62868c05c4b224b190e1ab63a23e467e99
AkashiNeko <akashineko@qq.com>
2025-03-14 20:20:39 +0800

    feat: Using the sh command with a Docker container, making it accessible to anyone

--- cmds/sh.go
+++ cmds/sh.go
@@ -2,46 +2,73 @@ package cmds
 
 import (
 	"fmt"
-	"go-hurobot/config"
 	"go-hurobot/qbot"
 	"log"
-	"os"
 	"os/exec"
 	"strings"
 	"time"
 )
 
-var workingDir string = ""
+var workingDir string = "/home/qwq"
+
+func truncateString(s string) string {
+	const (
+		maxLines    = 10
+		maxChars    = 500
+		truncateMsg = "\n输出过长，已自动截断"
+	)
+
+	lineCount := strings.Count(s, "\n") + 1
+
+	if lineCount >= 11 {
+		index := 0
+		for i := 0; i < 10; i++ {
+			index = strings.Index(s[index:], "\n") + 1 + index
+			if index == 0 {
+				return s
+			}
+		}
+		return s[:index] + truncateMsg
+	}
+
+	if len(s) > maxChars {
+		return s[:maxChars] + truncateMsg
+	}
+
+	return s
+}
 
 func cmd_sh(c *qbot.Client, msg *qbot.Message, args *ArgsList) {
-	if msg.UserID != config.MasterID {
-		c.SendReplyMsg(msg, fmt.Sprintf("%s: Permission denied", args.Contents[0]))
+	if msg.GroupID == 0 {
+		c.SendReplyMsg(msg, "请在群内使用")
 		return
 	}
+
 	if args.Size <= 1 {
 		c.SendReplyMsg(msg, "Usage: sh <linux command>")
 		return
 	}
 
+	rawcmd := decodeSpecialChars(msg.Raw[3:])
+
 	if strings.HasPrefix(args.Contents[1], "cd") {
-		if args.Size > 2 {
-			absPath, err := exec.Command("realpath", strings.TrimSpace(workingDir)).Output()
-			if err != nil {
-				c.SendReplyMsg(msg, err.Error())
-				return
-			}
-			workingDir = string(absPath)
-		} else {
-			workingDir = os.Getenv("HOME")
+
+		absPath, err := exec.Command("docker", "exec", "-i", "-u", "1000",
+			"-w", workingDir, "ubuntu", "bash", "-c",
+			fmt.Sprintf("%s && pwd", rawcmd)).CombinedOutput()
+
+		if err != nil {
+			c.SendReplyMsg(msg, err.Error())
+			return
 		}
+
+		workingDir = strings.TrimSpace(string(absPath))
 		c.SendReplyMsg(msg, workingDir)
 		return
 	}
 
-	cmd := exec.Command("bash", "-c", strings.Join(args.Contents[1:], " "))
-	if workingDir != "" {
-		cmd.Dir = workingDir
-	}
+	cmd := exec.Command("docker", "exec", "-i", "-u", "1000",
+		"-w", workingDir, "ubuntu", "bash", "-c", rawcmd)
 
 	done := make(chan error, 1)
 	var output []byte
@@ -60,8 +87,8 @@ func cmd_sh(c *qbot.Client, msg *qbot.Message, args *ArgsList) {
 			c.SendReplyMsg(msg, fmt.Sprintf("%v\n%s", err, string(output)))
 			return
 		}
-		c.SendReplyMsg(msg, string(output))
-	case <-time.After(30 * time.Second):
+		c.SendReplyMsg(msg, truncateString(string(output)))
+	case <-time.After(300 * time.Second):
 		cmd.Process.Kill()
 		c.SendReplyMsg(msg, fmt.Sprintf("命令执行超时: %s",
 			strings.Join(args.Contents[1:], " ")))
