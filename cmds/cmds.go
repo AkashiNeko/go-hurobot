93002f1c05749e5dabefe397ab7d564de3b42ef0
lavacreeper <hxh@cock.li>
2025-06-15 23:19:21 +0800

    fix: change command 'er' to 'fx'

--- cmds/cmds.go
+++ cmds/cmds.go
@@ -34,7 +34,7 @@ func init() {
 		"debug":        cmd_debug,
 		"essence":      cmd_essence,
 		"draw":         cmd_draw,
-		"er":           cmd_er,
+		"fx":           cmd_er,
 		"stock":        cmd_stock,
 		"crypto":       cmd_crypto,
 	}
