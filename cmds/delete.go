6f385163a9c6338c0e2389da19396b48a93da046
lavacreeper <hxh@cock.li>
2025-08-01 10:05:35 +0800

    fix:Prevent someone from abusing this feature

--- cmds/delete.go
+++ cmds/delete.go
@@ -3,10 +3,19 @@ package cmds
 import (
 	"go-hurobot/qbot"
 	"log"
+	"math/rand"
 	"strconv"
 )
 
 func cmd_delete(c *qbot.Client, msg *qbot.Message, args *ArgsList) {
+	// 禁止某个无锡人滥用删除功能，只有0.6%概率允许
+	if msg.UserID == 3112813730 {
+		if rand.Float64() > 0.006 {
+			c.SendMsg(msg, "无锡人本次运气不佳，删除失败！建议明天再试，或者考虑搬家")
+			return
+		}
+	}
+
 	if msg.Array[0].Type == qbot.Reply {
 		if msgid, err := strconv.ParseUint(msg.Array[0].Content, 10, 64); err == nil {
 			c.DeleteMsg(msgid)
